15ac0c1a756f9bb6c4d6e8bd64b5e175a06496f0
JanekBancarewicz <jan.bancarkiewicz@rb.com>
2021-02-19 19:56:49 +0100

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import Title from "./Title"
+import styled from "styled-components"
+import Image from "gatsby-image"
+import SearchButtons from "./SearchButtons"
+const Projects = ({ projects: data, title }) => {
+  const [projects, setProjects] = useState(data)
+  return (
+    <Wrapper className="section">
+      <Title title={title || "projects"}></Title>
+      <div className="section-center">
+        {projects.map((item) => {
+          const {id } = item
+          const { name, type } = item.data
+          const { fluid } = item.data.image.localFiles[0].childImageSharp
+          return (
+            <article key={id}>
+              <div className="container">
+                <Image fluid={fluid} className="img"/>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  background: var(--clr-grey-10);
+  .section-center {
+    margin-top: 4rem;
+    max-width: var(--max-width);
+    display: grid;
+    gap: 2rem;
+    /* safari workaround */
+    grid-gap: 2rem;
+    .img {
+      height: 20rem;
+      border-radius: var(--radius);
+      transition: var(--transition);
+    }
+    article {
+      box-shadow: var(--light-shadow);
+      border-radius: var(--radius);
+      transition: var(--transition);
+    }
+    article:hover {
+      box-shadow: var(--dark-shadow);
+    }
+    .container {
+      position: relative;
+      overflow: hidden;
+      border-radius: var(--radius);
+      background: var(--clr-primary-7);
+      &:hover .img {
+        opacity: 0.2;
+      }
+      .info {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+        opacity: 0;
+        transition: var(--transition);
+        color: var(--clr-white);
+        text-align: center;
+        p {
+          margin-bottom: 0.5rem;
+          color: var(--clr-white);
+          text-transform: uppercase;
+        }
+      }
+      &:hover .info {
+        opacity: 1;
+      }
+    }
+    @media (min-width: 768px) {
+      .img {
+        height: 15rem;
+      }
+      grid-template-columns: 1fr 1fr;
+    }
+    @media (min-width: 992px) {
+      .img {
+        height: 12.5rem;
+      }
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+    @media (min-width: 1200px) {
+      .img {
+        height: 15rem;
+      }
+    }
+  }
+  a {
+    display: block;
+    width: 9rem;
+    text-align: center;
+    margin: 0 auto;
+    margin-top: 3rem;
+  }
+`
+export default Projects
---
+++ src/pages/index.js
@@ -0,0 +1,57 @@
+import React from "react"
+import { graphql } from "gatsby"
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from "../components"
+import SEO from "../components/seo"
+const HomePage = ({ data }) => {
+  console.log(data)
+
+  const {
+    allAirtable: { nodes: projects },
+  } = data
+  console.log(projects)
+  return (
+    <Layout>
+      <Hero />
+      <About />
+      <Projects projects={projects} title="latest projects" />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 3
+      sort: { fields: data___date, order: DESC }
+    ) {
+      nodes {
+        data {
+          date
+          name
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
+export default HomePage
